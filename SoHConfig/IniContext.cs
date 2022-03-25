90cea9824ca39a55f6f7c3048bf5ae22d68827d3
robmikh <rob.mikh@outlook.com>
2022-03-24 23:13:39 -0700

    fix #2

--- SoHConfig/IniContext.cs
+++ SoHConfig/IniContext.cs
@@ -77,6 +77,7 @@ namespace SoHConfig
                     else
                     {
                         endIndex = i;
+                        break;
                     }
                 }
             }
